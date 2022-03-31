05ca24c97968dfa4b0e06bb327446315d072bdaa
foundify <foundifyinfo@gmail.com>
2022-03-30 23:51:16 -0400

    Contact Form

---
+++ site/assets/landing.js
@@ -0,0 +1,11 @@
+$('#landingSubmit').click(function() {
+    var value = $('input[name=option]:checked').val();
+    if (value == 'Basic') {
+      console.log(value);
+      window.open('https://square.link/u/DGJdI0bb','_blank');
+    }
+    else if (value == 'Advanced') {
+      console.log(value);
+      window.open('https://square.link/u/k59DiHnv','_blank');
+    }
+  });
