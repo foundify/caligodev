d2c74f81d99b63797c4a871c6a5924c3d3aa13ec
foundify <foundifyinfo@gmail.com>
2022-04-04 13:05:21 -0400

    New Links

--- site/assets/landing.js
+++ site/assets/landing.js
@@ -2,10 +2,10 @@ $('#landingSubmit').click(function() {
     var value = $('input[name=option]:checked').val();
     if (value == 'Basic') {
       console.log(value);
-      window.open('https://square.link/u/DGJdI0bb','_blank');
+      window.open('https://square.link/u/XoQ884Vy','_blank');
     }
     else if (value == 'Advanced') {
       console.log(value);
-      window.open('https://square.link/u/k59DiHnv','_blank');
+      window.open('https://square.link/u/YtCWtArP','_blank');
     }
   });
